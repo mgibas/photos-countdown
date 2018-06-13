64c392365790ae2df834cdb77ba98aa11c3e1ec9
Maciej Gibas <gibasmaciej@gmail.com>
2018-06-13 10:50:49 +0200

    Working counter

--- src/counter.js
+++ src/counter.js
@@ -5,19 +5,36 @@ import style from './counter.scss'
     constructor(el) {
       this.root = el
       this.root.classList.add(style.counter)
-      this.render() 
+      this.render(this._getDaysLeft()) 
     }  
-    render () {
-      console.log(style)
+    render (left) {
       let template = ` 
-        <div>
-        hello
+        <div class="${style.display} ${style.display1}" digit="${left[0]}">
+          ${ Array(28).join(0).split(0).map((item, i) => {
+            let rotation = Math.floor(Math.random() * 10) + 1 
+            rotation *= Math.floor(Math.random()*2) == 1 ? 1 : -1;
+            return `<div class="${style.cell} cell-${i}" style="transform: rotate(${rotation}deg);"></div>`
+          }).join('')}  
+        </div>
+        <div class="${style.display} ${style.display2}" digit="${left[1]}">
+          ${ Array(28).join(0).split(0).map((item, i) => {
+            let rotation = Math.floor(Math.random() * 10) + 1 
+            rotation *= Math.floor(Math.random()*2) == 1 ? 1 : -1;
+            return `<div class="${style.cell} cell-${i}" style="transform: rotate(${rotation}deg);"></div>`
+          }).join('')}  
         </div>
         `
       this.root.innerHTML = template
     }
+    _getDaysLeft() {
+      var oneDay = 24*60*60*1000;
+      var secondDate = new Date(new Date().getFullYear(),6,1);
+      var firstDate = new Date();
+      let left = Math.round(Math.abs((firstDate.getTime() - secondDate.getTime())/(oneDay)));
+      left = left.toString().length > 1 ? left.toString() : ' ' + left.toString() 
+      return left
+    }
   }
 
   document.querySelectorAll('counter').forEach((el)=> new Counter(el))
-//window.customElements.define('poc-component', PocComponent)
 }())
