876a07012a46a683d68acad7404ba52db5a64543
Maciej Gibas <gibasmaciej@gmail.com>
2018-06-14 14:56:53 +0200

    Something works

--- src/counter.js
+++ src/counter.js
@@ -10,21 +10,24 @@ import style from './counter.scss'
     render (left) {
       let template = ` 
         <div class="${style.display} ${style.display1}" digit="${left[0]}">
-          ${ Array(28).join(0).split(0).map((item, i) => {
-            let rotation = Math.floor(Math.random() * 10) + 1 
-            rotation *= Math.floor(Math.random()*2) == 1 ? 1 : -1;
-            return `<div class="${style.cell} cell-${i}" style="transform: rotate(${rotation}deg);"></div>`
-          }).join('')}  
+          ${ Array(28).join(0).split(0).map((item, i) => `
+            <div class="counter-cell ${style.cell} ${style['cell' + i]}" style="transform: rotate(${this._randomRotation()}deg);">
+            </div>
+          `).join('')}  
         </div>
         <div class="${style.display} ${style.display2}" digit="${left[1]}">
-          ${ Array(28).join(0).split(0).map((item, i) => {
-            let rotation = Math.floor(Math.random() * 10) + 1 
-            rotation *= Math.floor(Math.random()*2) == 1 ? 1 : -1;
-            return `<div class="${style.cell} cell-${i}" style="transform: rotate(${rotation}deg);"></div>`
-          }).join('')}  
+          ${ Array(28).join(0).split(0).map((item, i) => `
+            <div class="counter-cell ${style.cell} ${style['cell' + i]}" style="transform: rotate(${this._randomRotation()}deg);">
+            </div>
+          `).join('')}  
         </div>
         `
       this.root.innerHTML = template
+    }
+    _randomRotation() {
+      let rotation = Math.floor(Math.random() * 10) + 1 
+      rotation *= Math.floor(Math.random()*2) == 1 ? 1 : -1;
+      return rotation 
     }
     _getDaysLeft() {
       var oneDay = 24*60*60*1000;
